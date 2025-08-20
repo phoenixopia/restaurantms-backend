22125ece946b16193bf06f3051055a6c09ef1263
Ketsebaot Ertumo <ertumoketsebaot@gmail.com>
2025-08-20 17:05:06 +0300

    Remove deprecated routes and utility files, including activity log, admin routes, user management, and various utility functions. Clean up seeders and configuration files to streamline the project structure.

--- utils/idGenerator.js
+++
@@ -1,8 +0,0 @@
-// utils/idGenerator.js
-
-const { customAlphabet } = require('nanoid');
-const generateId = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 21);
-
-module.exports = {
-  getGeneratedId: () => generateId()
-};
