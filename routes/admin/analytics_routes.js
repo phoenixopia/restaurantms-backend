c7d82fa782e03586cfae5c1ca4ef966f522b6fc2
kelemnati <natikeleme1@gmail.com>
2025-09-19 09:09:22 +0300

    `Added two new routes and corresponding controller methods for viewing analytics snapshots as super admin and admin side.`

--- routes/admin/analytics_routes.js
+++ routes/admin/analytics_routes.js
@@ -1,7 +1,23 @@
 const express = require("express");
+
+const AnalyticsSnapshotController = require("../../controllers/admin/analytics_snapshot_controller");
 const { permissionCheck } = require("../../middleware/permissionCheck");
 const { authorize } = require("../../middleware/authorize");
 const { protect } = require("../../middleware/protect");
 const router = express.Router();
+
+router.get(
+  "/view-analytics-super",
+  protect("user"),
+  authorize("super_admin"),
+  AnalyticsSnapshotController.viewAnalyticsSuperAdmin
+);
+
+router.get(
+  "/view-analytics-admin",
+  protect("user"),
+  authorize("restaurant_admin"),
+  AnalyticsSnapshotController.viewAnalyticsAdminSide
+);
 
 module.exports = router;
